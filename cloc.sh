f0417b1b84f632dae8fe5942ae8f01815282ae73
Rahul Dinesh <94444265+kadavilrahul@users.noreply.github.com>
2025-06-13 23:23:13 +0530

    Create cloc.sh

---
+++ cloc.sh
@@ -0,0 +1,7 @@
+# Count Lines of Code
+# cloc is a popular tool that counts lines of code, blank lines, and comments by language.
+
+sudo apt-get install cloc
+
+echo "cloc /path/to/folder"
+echo "It gives a detailed breakdown by language and total lines"
